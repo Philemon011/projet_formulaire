8356e7116f90df77bd3f656d828b993026d336f4
Philemon <etoundelight1@gmail.com>
2024-02-20 12:25:35 +0100

    Déconnexion de l'utilisateur après 5 min d'inactivité

--- accueil.php
+++ accueil.php
@@ -1,7 +1,22 @@
 <?php
     session_start();
     require 'dbcon.php';
-?>
+
+    // Définir le délai d'inactivité en secondes
+    $inactivity_timeout = 300; // 5 minutes en secondes
+    // Déterminer l'heure actuelle
+    $current_time = time();
+    // Déconnexion si l'utilisateur est inactif depuis plus de 5 minutes
+    if (isset($_SESSION['last_activity']) && ($current_time - $_SESSION['last_activity']) > $inactivity_timeout) {
+        session_destroy();
+        header('Location: login.php');
+    }
+    
+    // Mise à jour de l'heure de la dernière activité
+    $_SESSION['last_activity'] = $current_time;
+  
+  ?>
+  
 
 <!DOCTYPE html>
 <html lang="fr">
